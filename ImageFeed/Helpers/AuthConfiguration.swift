e2377dd2398fbe5b3182ff844751a3c64bb5684e
amina19058 <a.khusnutdinova@innopolis.university>
2025-04-16 17:17:52 +0300

    fix constants

--- ImageFeed/Helpers/AuthConfiguration.swift
+++ ImageFeed/Helpers/AuthConfiguration.swift
@@ -8,13 +8,22 @@
 import Foundation
 
 enum Constants {
-    static let accessKey = "your_Access_Key"
-    static let secretKey = "your_Secret_Key"
-    static let redirectURI = "your_Redirect_URI"
+    static let accessKey = "OZHrlUzJ1u1KmlqhgUNgsq4wZZRyAz0w3Wm2QyknfYg"
+    static let secretKey = "_YvMrBEuNYkDlMKNIS39Ximp06X6J8d6BihIeqAP85g"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
-
     static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    
     static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+}
+
+enum UnsplashUrlStrings {
+    static let token = "https://unsplash.com/oauth/token"
+    static let authorize = "https://unsplash.com/oauth/authorize"
+    static let profile = "https://api.unsplash.com/me"
+    static let profileImage = "https://api.unsplash.com/users/"
+    static let photos = "https://api.unsplash.com/photos"
+
 }
 
 struct AuthConfiguration {
@@ -42,14 +51,5 @@ struct AuthConfiguration {
                                  authURLString: Constants.unsplashAuthorizeURLString,
                                  defaultBaseURL: Constants.defaultBaseURL)
     }
-
-}
-
-enum UnsplashUrlStrings {
-    static let token = "https://unsplash.com/oauth/token"
-//    static let authorize = "https://unsplash.com/oauth/authorize"
-    static let profile = "https://api.unsplash.com/me"
-    static let profileImage = "https://api.unsplash.com/users/"
-    static let photos = "https://api.unsplash.com/photos"
 
 }
